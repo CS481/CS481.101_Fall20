540ffaae0bee9fe90b046db1b0b8f7a1ab801b4f
Cayden Reynolds <cayden@arclasr.net>
2020-10-05 12:11:57 -0400

    Fixed imports

--- simulationfactory/src/pages/Accountpage.js
+++ simulationfactory/src/pages/Accountpage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CreateStyles from "../util/stylesheet";
+import CreateStyles from "../util/Stylesheet";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
