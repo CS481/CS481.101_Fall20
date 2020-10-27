d15b2e50cb3b5459764b210c7d696a5b2d1df74e
Cayden Reynolds <cayden@arclasr.net>
2020-10-27 17:11:17 -0400

    Playerpage works, kinda

--- simulationfactory/src/pages/Factorypage.js
+++ simulationfactory/src/pages/Factorypage.js
@@ -4,9 +4,9 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
 import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 import Table from '../components/Table'
-
 
 import {
   Tabs,
