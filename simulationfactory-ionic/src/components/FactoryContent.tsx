73c1dfba2c2d07f3ee84f166fcf3ca7892a98558
alouderback <alex@alouderback.com>
2021-02-27 14:24:44 -0500

    first two slides created!

--- simulationfactory-ionic/src/components/FactoryContent.tsx
+++ simulationfactory-ionic/src/components/FactoryContent.tsx
@@ -1,12 +1,59 @@
-import { IonContent, IonLabel } from "@ionic/react";
-import React from "react";
+import { IonContent, IonInput, IonItem, IonLabel, IonList, IonListHeader, IonSlide, IonSlides, IonTextarea } from "@ionic/react";
+import React, { useState} from "react";
 
-import './HomeContent.css';
+import './FactoryContent.css';
 
 const FactoryContent: React.FC = () => {
+    const [title,setTitle] = useState<string>();
+    const [desc,setDesc] = useState<string>();
+    const [numPlayers, setNumPlayers] = useState<number>(0);
+    const [playerTitle, setPlayerTitle] = useState<string>();
+    
+    const slideOpts = {
+        slidesPerView: 1,
+        initialSlide: 0
+    };
+
     return (
     <IonContent className="ion-padding">
-        <IonLabel color="primary">WELCOME TO THE FACTORY</IonLabel>
+        <IonSlides pager={true} options={slideOpts}>
+            <IonSlide>
+                <IonList className="container">
+                    <IonListHeader>
+                        <IonLabel><strong>Simulation Title</strong></IonLabel>
+                    </IonListHeader>
+                    <IonItem>
+                        <IonLabel position="floating">Simulation Title</IonLabel>
+                        <IonInput value={title} onIonChange={e => setTitle(e.detail.value!)}></IonInput>
+                    </IonItem>
+                    <IonListHeader>
+                        <IonLabel><strong>Simulation Description</strong></IonLabel>
+                    </IonListHeader>
+                    <IonItem>
+                        <IonLabel position="floating">Simulation Description</IonLabel>
+                        <IonTextarea value={desc} onIonChange={e => setDesc(e.detail.value!)}></IonTextarea>
+                    </IonItem>
+                </IonList>
+            </IonSlide>
+            <IonSlide>
+                <IonList className="container">
+                    <IonListHeader>
+                        <IonLabel><strong>Number of Players</strong></IonLabel>
+                    </IonListHeader>
+                    <IonItem>
+                        <IonLabel position="floating">Number of Players</IonLabel>
+                        <IonInput type="number" value={numPlayers} onIonChange={e => setNumPlayers(parseInt(e.detail.value!, 10))}></IonInput>
+                    </IonItem>
+                    <IonListHeader>
+                        <IonLabel><strong>Player Title</strong></IonLabel>
+                    </IonListHeader>
+                    <IonItem>
+                        <IonLabel position="floating">Player Title</IonLabel>
+                        <IonInput value={playerTitle} onIonChange={e => setPlayerTitle(e.detail.value!)}></IonInput>
+                    </IonItem>
+                </IonList>
+            </IonSlide>
+        </IonSlides>
     </IonContent>
     );
 };
