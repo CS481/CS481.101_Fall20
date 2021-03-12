e375622366c604cbc63be47a9eca609c898745b2
acastro7 <48137353+acastro7@users.noreply.github.com>
2021-03-12 09:47:51 -0500

    Worked on Join simulation
    I filled out more content for join simulation. I just need a little more guidance on what join simulation needs to be finished.

--- simulationfactory-ionic/src/components/PlayerContent.tsx
+++ simulationfactory-ionic/src/components/PlayerContent.tsx
@@ -1,8 +1,9 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonInput, IonCardTitle, IonCol, IonContent, IonGrid, IonItem, IonLabel, IonList, IonRippleEffect, IonRow } from "@ionic/react";
-import React, {useState } from "react";
-import { File } from '@ionic-native/file';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonInput, IonCardTitle, IonCol, IonContent, IonGrid, IonItem, IonLabel, IonList, IonRippleEffect, IonRow, IonSlide, IonSlides, IonTextarea } from "@ionic/react";
+import React, {useRef, useState } from "react";
 
 import './PlayerContent.css';
+import {prompt, user_count, round_count,resources} from './Info.json';
+//import { BeginSim } from "./../util/Backend";
 
 
 
@@ -11,55 +12,116 @@ const PlayerContent: React.FC = () => {
     const [username,setUsername] = useState<string>();
     const [password, setPassword] = useState<string>();
     const [simulation_id, setSimulationID] = useState<number>();
+    const [userResponse, setUserResponse] = useState<string>();
 
 const jsonData = '{"simulation_id":'+username +', "password":'+password+', "username":'+username;
-function writeJSON(){
+const playerSlides = useRef(document.createElement('ion-slides'));
+
+const next = () =>{
+    playerSlides.current.slideNext();
     
-    console.log("This is a message\n");
+}
+
+//'this' doesn't work in ionic .tsx need to find a replacement
+// const beginSim= () =>{
+//     BeginSim(this.getSimulationInstance(), () => {
+//         this.setState({logged_in: true});
+//         this.setSimState();
+//     });
+// }
+
+//will be used once I get beginSim to work
+const submitResponse = () =>{
+    //empty method
+
+    //end the method by sending the user to next slide
+    next();
 }
 
     return (
     <IonContent className="ion-padding">
-        <IonGrid>
+        <IonGrid>   
             <IonRow className="ion-justify-content-center">
                 <IonCol className="ion-text-center">
-                    <IonCard>
-                        <IonCardHeader color="primary">
-                            <IonCardTitle>Simulation Player</IonCardTitle>
-                        </IonCardHeader>
-                        <IonCardContent>
-                            <IonList lines="none">
-                                <IonItem>
-                                    <IonLabel className="ion-text-center">Enter Your Player credentials</IonLabel>
-                                </IonItem>
-                                <IonItem>
-                                    <IonInput value={username} placeholder="Username"onIonChange={e => setUsername(e.detail.value!)}></IonInput>                                    
-                                </IonItem>
+                    <IonSlides ref={playerSlides}>
+                        <IonSlide class="swiper-no-swiping">
 
-                                <IonItem>
-                                    <IonLabel>{username}</IonLabel>                                    
-                                </IonItem>
+                            <IonCard className="container">
+                                <IonCardHeader color="primary">
+                                    <IonCardTitle>Simulation Player</IonCardTitle>
+                                </IonCardHeader>
+                                <IonCardContent>
+                                    <IonList lines="none">
+                                        <IonItem>
+                                            <IonLabel className="ion-text-center">Enter Your Player credentials</IonLabel>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonInput value={username} placeholder="Username"onIonChange={e => setUsername(e.detail.value!)}></IonInput>                                    
+                                        </IonItem>
 
-                                <IonItem>
-                                    <IonInput value={password} placeholder="Password" onIonChange={e => setPassword(e.detail.value!)}></IonInput>
-                                </IonItem>
+                                        <IonItem>
+                                            <IonLabel>{username}</IonLabel>                                    
+                                        </IonItem>
 
-                                <IonItem>
-                                    <IonInput value={simulation_id} placeholder="SimulationID" onIonChange={e => setSimulationID(parseInt(e.detail.value!, 10))}></IonInput>
-                                </IonItem>
+                                        <IonItem>
+                                            <IonInput value={password} placeholder="Password" onIonChange={e => setPassword(e.detail.value!)}></IonInput>
+                                        </IonItem>
+
+                                        <IonItem>
+                                            <IonInput value={simulation_id} placeholder="SimulationID" onIonChange={e => setSimulationID(parseInt(e.detail.value!, 10))}></IonInput>
+                                        </IonItem>
+                                        
+                                    </IonList> 
+
+                                    <IonButton routerLink="/page/player" routerDirection="root">
+                                        Begin
+                                        <IonRippleEffect></IonRippleEffect>
+                                    </IonButton>
+                                    <IonButton onClick={() => next()}>Add Resource</IonButton>
+
+                                </IonCardContent>
+                            </IonCard>
+                        </IonSlide>
+
+                        <IonSlide class="swiper-no-swiping">
+                            <IonCard className="container">
+                                <IonCardHeader color="primary">
+                                    <IonCardTitle>Simulation {username} Player</IonCardTitle>
+                                </IonCardHeader>
+                                <IonCardContent>
+                                    <IonList lines="none">
+                                        <IonItem>
+                                            <IonLabel>Simulation Question: {prompt}</IonLabel>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel>Number of rounds: {round_count}</IonLabel>
+                                            <IonLabel>Number of past users: {user_count}</IonLabel>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel>Please enter response:</IonLabel>
+                                        </IonItem>
+
+                                        <IonItem>
+                                        <IonTextarea value={userResponse} placeholder="Response" onIonChange={e => setUserResponse(e.detail.value!)}></IonTextarea>
+                                        </IonItem>
+                                    </IonList>
+                                    <IonButton onClick={() => submitResponse()}>Submit</IonButton>
+                                </IonCardContent>
+                            </IonCard>
+                        </IonSlide>
+
+                        <IonSlide class="swiper-no-swiping"> 
+                            <IonCard className="container">
+                                <IonCardHeader color="primary">
+                                    <IonCardTitle>Thank you for participating</IonCardTitle>
+                                </IonCardHeader>
                                 
-                            </IonList> 
+                            </IonCard>
+                        </IonSlide>
 
-                            <IonButton routerLink="/page/player" routerDirection="root">
-                                Begin
-                                <IonRippleEffect></IonRippleEffect>
-                            </IonButton>
-                            <IonButton onClick={ () => writeJSON}>Add Resource</IonButton>
+                    </IonSlides>
 
-                        </IonCardContent>
-                    </IonCard>
                 </IonCol>
-
             </IonRow>
         </IonGrid>
     </IonContent>
