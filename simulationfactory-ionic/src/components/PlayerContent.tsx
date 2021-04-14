e0d6d99b627832accf4a3b989a38ed22cda3811d
acastro7 <48137353+acastro7@users.noreply.github.com>
2021-04-14 12:23:30 -0400

    player content changes
    My work before i delete it

--- simulationfactory-ionic/src/components/PlayerContent.tsx
+++ simulationfactory-ionic/src/components/PlayerContent.tsx
@@ -2,10 +2,8 @@ import { IonButton, IonCard, IonCardContent, IonRefresher, IonRefresherContent, 
 import React, {useRef, useState } from "react";
 
 import './PlayerContent.css';
-//import {prompt, user_count, round_count,resources, _id} from './Info.json';
 import { BeginSim, SubmitResponse } from "./../util/Backend";
 import { RefresherEventDetail } from '@ionic/core';
-import { chevronDownCircleOutline } from 'ionicons/icons';
 
 const PlayerContent: React.FC = () => {
     //Javascript
@@ -17,16 +15,14 @@ const PlayerContent: React.FC = () => {
     //Sim variables
     const [turnNumber, setTurnNumber] = useState<number>();
     const [prompt, setPrompt] = useState<string>();
-    const [responses, setResponses] = useState([
-        "200"
-    ]);
+    const [responses, setResponses] = useState<number[]>();
 
 
     const [userID, setUserID] = useState<string>();
     const [history, setHistory] = useState<string[]>();
     const [userHistory, setUserHistory] = useState<string[]>();
     const [userWaiting, setUserWaiting] = useState<boolean>();    
-    var SimResponses = [""];
+    var SimResponses = [1];
 
 const userData = {'user':{'username':username, 'password':password}, 'id':simulation_id};
 const playerSlides = useRef(document.createElement('ion-slides'));
@@ -38,27 +34,11 @@ function previous(){
     playerSlides.current.slidePrev();
 }
 const verify = () =>{
-    if ( password === "Ad3$5asdf" && username === "me"  ) {
-        //(simulation_id ==="a") ? next() : console.log("Error incorrect simulation Id");
-    }
-    
-    else{
-        console.log("Error incorrect simulation Id");
-    }
+    //next();
+   
     StartSim();
     
     
-}
-
-//method that dynamically writes all the radio buttons
-function createOptions() {
-    var maxRadioButtons = 7;
-    for(var i = 0; i < maxRadioButtons; i++){
-        //loop that writes ion http
-        //close but this completely overwrites the page
-        // document.write("Hello world");
-        // document.write("<IonLabel className='ion-text-center'>Enter Your Player credentials</IonLabel>") 
-    }
 }
 
 function doRefresh() {
@@ -69,8 +49,7 @@ function doRefresh() {
   }
 
 
-//'this' doesn't work in ionic .tsx need to find a replacement
-//pass in the in memory version of a json for this, should just be one object
+//Back end function specifically for join simulation
 function StartSim(){
     /*input state variable with response */
     try{
@@ -91,18 +70,18 @@ function InitSim (response){
     
     setTurnNumber(response.turn_number);
     setPrompt(response.prompt);
-
-    for(let i = 0; i < response.responses.length; i++){
-        //setResponses(responses => [...responses, response.responses[i]]);
-    }
     SimResponses = response.responses.sort();
-    setResponses(response.responses);
+
+    // for(let i = 0; i < response.responses.length; i++){
+    //     setResponses(responses => responses.concat(SimResponses) )
+    // }
+    setResponses(responses => [...responses, response.responses.sort()]);
     
     setUserID(response.user_id);
     setHistory(response.history);
     setUserWaiting(response.user_waiting);
 
-    console.log("Testerrrr----------- +"+ userWaiting);
+    console.log("Testerrrr-----999s------ +"+ history);
     console.log("tesssssss "+ SimResponses);
     console.log("Mooooooo ----- " + responses)
     
@@ -184,59 +163,28 @@ function SubmitCallBack(){
                                 <IonCardContent>
                                     <IonList lines="none">
                                         <IonItem>
+                                            <IonLabel>The environment resource is: {/*History resources */}</IonLabel>
+                                            <IonLabel>{/*first players resource name or function call */} is at---</IonLabel><IonLabel>{/*first players resource name or function call */} is at---</IonLabel>
+                                        </IonItem>
+                                        <IonItem>
                                             <IonLabel>Simulation Prompt: {prompt}</IonLabel>
                                         </IonItem>
                                         <IonItem>
-                                            <IonLabel>Current round: {turnNumber}</IonLabel>
+                                            <IonLabel slot="start">How would you like to affect your production</IonLabel>
+                                            <IonLabel slot="end">Current round: {turnNumber}</IonLabel>
                                         </IonItem>
-                                        {
-                                                                               
-                                            <IonRadioGroup value={response} onIonChange={e => setResponse(e.detail.value)}>
-                                            <IonListHeader>
-                                                <IonHeader>Please enter response</IonHeader>
-                                            </IonListHeader>
-                                            
-                                            <IonItem>
-                                                <IonLabel>15</IonLabel>
-                                                <IonRadio slot="start" color="tertiary" value="15"></IonRadio>
-                                            </IonItem>
-                                            <IonItem>
-                                                <IonLabel>10</IonLabel>
-                                                <IonRadio slot="start" color="tertiary" value="10"></IonRadio>
-                                            </IonItem>
-                                            <IonItem>
-                                                <IonLabel>5</IonLabel>
-                                                <IonRadio slot="start" color="tertiary" value="5"></IonRadio>
-                                            </IonItem>
-                                            <IonItem>
-                                                <IonLabel>0</IonLabel>
-                                                <IonRadio slot="start" color="tertiary" value="0"></IonRadio>
-                                            </IonItem>
-                                            <IonItem>
-                                                <IonLabel>-5</IonLabel>
-                                                <IonRadio slot="start" color="tertiary" value="-5"></IonRadio>
-                                            </IonItem>
-                                            <IonItem>
-                                                <IonLabel>-10</IonLabel>
-                                                <IonRadio slot="start" color="tertiary" value="-10"></IonRadio>
-                                            </IonItem>
-                                            <IonItem>
-                                                <IonLabel>-15</IonLabel>
-                                                <IonRadio slot="start" color="tertiary" value="-15"></IonRadio>
-                                            </IonItem>
-                                                
-                                            </IonRadioGroup>        
-                                        }
 
-<IonItemDivider>Default</IonItemDivider>
-          <IonItem>
-            <IonRange pin={true} min={-5} max={5} snaps onIonChange={e => setResponse(e.detail.value.toString())}/>
-            <IonLabel slot="start" color="tertiary">{SimResponses[0]}</IonLabel>
-            <IonLabel slot="end" color="tertiary">{/*responses[responses.length-1]*/}</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Value: {response}</IonLabel>
-          </IonItem>
+                                    { /* Remember this divider for the future.
+                                    <IonItemDivider>Default</IonItemDivider> 
+                                    */}
+                                    <IonItem>
+                                        <IonRange pin={true} min={-5} max={5} snaps onIonChange={e => setResponse(e.detail.value.toString())}/>
+                                        <IonLabel slot="start" color="tertiary">min</IonLabel>
+                                        <IonLabel slot="end" color="tertiary">Max</IonLabel>
+                                    </IonItem>
+                                    <IonItem>
+                                        <IonLabel>Value: {response}</IonLabel>
+                                    </IonItem>
 
                                     </IonList>
                                     <IonButton onClick={() => SubmitRes()}>Submit</IonButton>
